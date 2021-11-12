ea212db2b7e31d43ce184bb3f52c1ee302813fa7
Basilio40 <42722896+Basilio40@users.noreply.github.com>
2021-11-12 13:11:45 -0300

    LENDO PDF DE PONTO

--- funcionarios.py
+++ funcionarios.py
@@ -1,6 +1,8 @@
 import PyPDF2
+import tabula
 from PyPDF2.pdf import PdfFileReader, PdfFileWriter
 
+#=======================================LENDO DADOS DOS HOLERITES DOS FUNCIONARIOS===================================================
 class CarregarPDF:
     def __init__(self):
         self.holerite = r"C:\Users\Sandro Bispo\Desktop\projeto_vetor\Holerite\HOLERITES 10-2020.pdf"
@@ -11,7 +13,7 @@ class CarregarPDF:
         self.conteudo = self.pagina.extractText()
         # print(self.conteudo)
         
-    
+#========================================== DIVIDINDO O CARTÃO DE POR FUNCIONARIO ====================================================
     def split_pdf(ponto):
         ponto = PdfFileReader(open(r"C:\Users\Sandro Bispo\Desktop\projeto_vetor\cartao_ponto\Relatório de Jornada (espelho ponto) (26.10.2021- 02.11.2021).pdf", 'rb'))
         for p in range(ponto.numPages):
@@ -19,8 +21,13 @@ class CarregarPDF:
             saida.addPage(ponto.getPage(p))
             with open("saidaponto/ponto-page%s.pdf" %p, "wb") as saidaStream:
                 saida.write(saidaStream)
+                
+#======================================= LENDO OS DADOS DO CARTÃO DE PONTO ===========================================================              
+    def read_ponto(self):
+        self.funcionario = tabula.read_pdf(r"C:\Users\Sandro Bispo\Desktop\projeto_vetor\saidaponto\ponto-page0.pdf", pages='all')
+        print(self.funcionario)
   
 carregar = CarregarPDF()
-print(carregar.carregaholerite())
+carregar.carregaholerite()
 carregar.split_pdf()
-
+carregar.read_ponto()
