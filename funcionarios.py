805aec9827f352671c675e72a204b8947df50d4d
Basilio40 <42722896+Basilio40@users.noreply.github.com>
2021-11-16 11:08:29 -0300

    Alterações no split

--- funcionarios.py
+++ funcionarios.py
@@ -2,39 +2,35 @@ import PyPDF2
 import tabula
 from PyPDF2.pdf import PdfFileReader, PdfFileWriter
 
+
 #=======================================LENDO DADOS DOS HOLERITES DOS FUNCIONARIOS===================================================
 class CarregarPDF:
     def __init__(self):
-        self.holerite = r"C:\Users\Sandro Bispo\Desktop\projeto_vetor\Holerite\HOLERITES 10-2020.pdf"
+        self.holerite = r"C:\Users\Sandro Bispo\Desktop\Pythons_do_mes\projeto_vetor\Holerite\HOLERITES 10-2020.pdf"
 
     def carregaholerite(self):
         self.lerHolerite =  PyPDF2.PdfFileReader(self.holerite)
         self.pagina = self.lerHolerite.getPage(0)
         self.conteudo = self.pagina.extractText()
-        # print(self.conteudo)
+        print(self.conteudo)
                       
-#======================================= LENDO OS DADOS DO CARTÃO DE PONTO ===========================================================              
-    def read_ponto(self):
-        self.funcionario = tabula.read_pdf(r"C:\Users\Sandro Bispo\Desktop\projeto_vetor\cartao_ponto\Relatório de Jornada (espelho ponto) (26.10.2021- 02.11.2021).pdf", pages='all')
-        for tabela in self.funcionario:
-            tabela = tabela.columns[0:1]
-            for i in tabela:
-                print(i)
-
-#========================================== DIVIDINDO O CARTÃO DE POR FUNCIONARIO ====================================================
-    # def split_pdf(ponto):
-    #     ponto = PdfFileReader(open(r"C:\Users\Sandro Bispo\Desktop\projeto_vetor\cartao_ponto\Relatório de Jornada (espelho ponto) (26.10.2021- 02.11.2021).pdf", 'rb'))
-    #     for p in range(ponto.numPages):
-    #         saida = PdfFileWriter()
-    #         saida.addPage(ponto.getPage(p))
-    #         with open("saidaponto/ponto-page%s.pdf" %p, "wb") as saidaStream:
-    #             saida.write(saidaStream)
+#========================================== LENDO E DIVIDINDO O CARTÃO DE POR FUNCIONARIO ====================================================
+    def split_pdf(ponto):
+        ponto = PdfFileReader(open(r"C:\Users\Sandro Bispo\Desktop\Pythons_do_mes\projeto_vetor\cartao_ponto\Relatório de Jornada (espelho ponto) (26.10.2021- 02.11.2021).pdf", 'rb'))
+        funcionarios = tabula.read_pdf(r"C:\Users\Sandro Bispo\Desktop\Pythons_do_mes\projeto_vetor\cartao_ponto\Relatório de Jornada (espelho ponto) (26.10.2021- 02.11.2021).pdf", pages='all')
+        for p in range(ponto.numPages):
+            saida = PdfFileWriter()
+            saida.addPage(ponto.getPage(p))
+            funcionario = funcionarios[p].columns[0:1]
+            with open(f"saidaponto/{funcionario[0]}.pdf".replace(':',''), "wb") as saidaStream:
+                saida.write(saidaStream)
  
+ #===========================================RENOMEANDO OS ARQUIVOS DE PONTOS ========================================================
                 
         
     
   
 carregar = CarregarPDF()
-carregar.carregaholerite()
-# carregar.split_pdf()
-carregar.read_ponto()
+# carregar.carregaholerite()
+carregar.split_pdf()
+
